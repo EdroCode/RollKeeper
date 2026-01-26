ea1ae583d485c1670541b85480a615fe898ebd3f
PedroCoutinho <edr0c0de@protonmail.com>
2026-01-26 23:50:05 +0000

    feat: remade turn order

--- src/app.js
+++ src/app.js
@@ -5,7 +5,13 @@ const turnOrderTemplates = {
     turnLabel: `
     
         <li draggable="true" ondragover="dragOver(event)" ondblclick="this.remove()"  ondragstart="dragStart(event)" class="cursor-grab "><input class="border-b hover:border-red-700 pl-1 py-2 w-full cursor-grab" placeholder="Name"></li>
-    `
+    `,
+    preset2: `
+        <div class="flex gap-2">
+            <h1 class="flex border w-1/4 items-center justify-center rounded-lg">1</h1>
+            <input class="border-b pl-1 py-2 w-full cursor-text" placeholder="Name">
+        </div>
+        `
 }
 
 
@@ -176,40 +182,33 @@ const templates = {
     `,
 
     turnorder: `
-        <div id="turnorder" class="flex flex-col gap-1 bg-white border rounded-2xl p-2 border-black w-64 ">
-            <div class="flex items-center justify-between  gap-1">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
-                </svg>
-
-                <h1 class="text-lg font-semibold">Turn Order</h1>
-                <button class="cursor-pointer" onclick="deleteItem(this)">
+        <div id="turnorder" class="flex flex-col gap-1 bg-white border rounded-2xl p-2 border-black w-80 ">
+                <div class="flex items-center justify-between  gap-1">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M3 7.5 7.5 3m0 0L12 7.5M7.5 3v13.5m13.5 0L16.5 21m0 0L12 16.5m4.5 4.5V7.5" />
                     </svg>
-                </button>
-            </div>
-            <hr>
-
-            <div class="flex flex-col w-full h-full justify-between py-1 ">
-                
-                <div class="flex w-full m-1 pr-2">
-                
-                    <ul id="orderList">
-                        
-                        
-                    </ul>
+                    <h1 class="text-lg font-semibold">Turn Order</h1>
+                    <button class="cursor-pointer" onclick="deleteItem(this)">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                        </svg>
+                    </button>
                 </div>
+                <hr>
 
-                <button class="text-2xl cursor-pointer border rounded-xl w-full mt-2"
-                        onclick="addOrder(this)"
-                    >
-                        +
-                </button>
+                <div id="initiative-roll-panel" class="flex flex-col w-full h-full justify-between py-1 ">
                     
+                    <div id="initiative-container" class="flex flex-col w-full m-1 pr-2 gap-2">
+                        <div class='flex gap-2'>
+                            <h1 class='flex border p-2 w-1/6 items-center justify-center rounded-lg'>1</h1>
+                            <input class='border-b py-2 w-3/4 cursor-text outline-0' placeholder='Name'>
+                            <input class='flex border-b pl-1 py-2 w-1/4 cursor-text justify-center outline-0' placeholder='20' type='number'>
+                        </div>
+                    </div>
+                    
+                    <button class="text-2xl cursor-pointer border rounded-xl w-full mt-2" onclick="addOrder(this)">+</button>  
+                </div>
             </div>
-
-        </div>
     `,
 
     preset: `
@@ -255,13 +254,13 @@ function loadTemplate(name) {
         `;
     } 
     if (name == "turnorder") {
-        if (isUnique("turnorder")) {
-                newDiv = `
-                <div id="${customID}" class="h-min">
-                    ${add}
-                </div>
-            `;
-        }
+       
+        newDiv = `
+            <div id="${customID}" class="h-min">
+                ${add}
+            </div>
+        `;
+        
     }
     else {
         newDiv = `
@@ -401,35 +400,33 @@ function autoGrow(element) {
 
 // INITIATIVE TRACKER
 
-var _el;
 
-function dragOver(e) {
-  if (isBefore(_el, e.target))
-    e.target.parentNode.insertBefore(_el, e.target);
-  else
-    e.target.parentNode.insertBefore(_el, e.target.nextSibling);
+function addOrder(base){
+    
+    const container = base.closest('#initiative-roll-panel');
+    const divContainer = container.querySelector('div'); 
+    
+    const count = divContainer.querySelectorAll('div').length;
+    console.log(count)
+    
+    const add = `
+        <div class='flex gap-2'>
+            <h1 class='flex border p-2 w-1/6 items-center justify-center rounded-lg'>${count + 1}</h1>
+            <input class='border-b py-2 w-3/4 cursor-text outline-0' placeholder='Name'>
+            <input class='flex border-b pl-1 py-2 w-1/4 cursor-text justify-center outline-0' placeholder='20' type='number'>
+        </div>
+    `;
+
+    divContainer.insertAdjacentHTML('beforeend', add);
+
+                            
 }
 
-function dragStart(e) {
-  e.dataTransfer.effectAllowed = "move";
-  e.dataTransfer.setData("text/plain", null);
-  _el = e.target;
-}
-
-function isBefore(el1, el2) {
-  if (el2.parentNode === el1.parentNode)
-    for (var cur = el1.previousSibling; cur && cur.nodeType !== 9; cur = cur.previousSibling)
-      if (cur === el2)
-        return true;
-  return false;
-}
 
 
-function addOrder(base) {
-    const add = turnOrderTemplates["turnLabel"];
-    const container = base.closest('.flex.flex-col');
-    const listdiv = container.querySelector('#orderList');
+// LOCAL STORAGE
 
-    listdiv.insertAdjacentHTML('beforeend', add);
-}
+
+
+
 
